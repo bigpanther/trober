8ed3c5e3624b1799dfe0148f4cb180ef302d5a46
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-07 22:02:08 -0800

    Add log id to end user facing errors

--- models/errors.go
+++ models/errors.go
@@ -1,10 +1,21 @@
 package models
 
-import "github.com/gofrs/uuid"
+import (
+	"log"
+
+	"github.com/gofrs/uuid"
+)
 
 // CustomError
 type CustomError struct {
 	ID      uuid.UUID `json:"id"`
 	Message string    `json:"message"`
 	Code    string    `json:"code"`
+}
+
+//NewCustomError returns an instance of a custom error
+func NewCustomError(message string, code string, err error) CustomError {
+	id, _ := uuid.NewV4()
+	log.Println(id, code, message, "Err:", err)
+	return CustomError{ID: id, Code: code, Message: message}
 }

cbc406595573835b5c554de6056377fa051d8064
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-09 21:13:03 -0800

    Add sdk generation for the API Closes #6

---
+++ sdk/lib/trober.dart
@@ -0,0 +1,16 @@
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+    additionalProperties: AdditionalProperties(
+        pubName: 'trober_sdk',
+        pubAuthor: 'Big Panther Inc.',
+        pubVersion: '0.0.1',
+        pubAuthorEmail: 'info@bigpanther.ca',
+        pubDescription: 'The trober SDK',
+        pubHomepage: 'https://bigpanther.ca'),
+    inputSpecFile: 'lib/trober.yaml',
+    apiPackage: 'trober_sdk',
+    generatorName: Generator.DART2_API,
+    alwaysRun: true,
+    outputDirectory: 'trober_sdk')
+class Trober extends OpenapiGeneratorConfig {}

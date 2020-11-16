6d773afa24eaa7fbd95f304dce28ca41efa90a67
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-15 21:29:38 -0800

    replace put with patch Closes #21

--- main.go
+++ main.go
@@ -18,21 +18,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-/*
-# Notes about `main.go`
-
-## SSL Support
-
-We recommend placing your application behind a proxy, such as
-Apache or Nginx and letting them do the SSL heavy lifting
-for you. https://gobuffalo.io/en/docs/proxy
-
-## Buffalo Build
-
-When `buffalo build` is run to compile your binary, this `main`
-function will be at the heart of that binary. It is expected
-that your `main` function will start your application using
-the `app.Serve()` method.
-
-*/

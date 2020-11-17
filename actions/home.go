e5e6801287810a6a4d000e4f268eb4d17d209926
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-16 19:14:44 -0800

    Add version placeholder

--- actions/home.go
+++ actions/home.go
@@ -6,8 +6,11 @@ import (
 	"github.com/gobuffalo/buffalo"
 )
 
-// HomeHandler is a default handler to serve up
-// a home page.
+var (
+	version = "dev"
+	commit  = "dev"
+)
+
 func HomeHandler(c buffalo.Context) error {
 	return c.Render(http.StatusOK, r.JSON(map[string]string{"message": "Welcome to Trober!"}))
 }

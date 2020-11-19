6748ec1ec618d49f75ecab4ac53490c14ac96102
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-18 16:46:07 -0800

    Add version info at runtime

--- actions/home.go
+++ actions/home.go
@@ -2,13 +2,14 @@ package actions
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gobuffalo/buffalo"
 )
 
 var (
-	version = "dev"
-	commit  = "dev"
+	version = os.Getenv("TROBER_VERSION")
+	commit  = os.Getenv("TROBER_COMMIT")
 )
 
 func HomeHandler(c buffalo.Context) error {

165ed1789c70c65fa0a93ffd6a1cb6e59172f44f
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-16 20:01:53 -0800

    Add tests

--- actions/self_test.go
+++ actions/self_test.go
@@ -1,32 +1,51 @@
 package actions
 
 import (
+	"testing"
+
 	"github.com/bigpanther/trober/models"
 	"github.com/gobuffalo/httptest"
 )
 
 func (as *ActionSuite) Test_SefGet() {
-	user := as.getLoggedInUser("nicoleta")
+	as.LoadFixture("Tenant bootstrap")
+	user := as.getLoggedInUser("firmino")
 	req := as.setupRequest(user, "/self")
 	res := req.Get()
 	as.Equal(200, res.Code)
 	var self = &models.User{}
 	res.Bind(self)
-	as.Equal("nicoleta", self.Username)
+	as.Equal("firmino", self.Username)
 }
 
 func (as *ActionSuite) Test_SefGetTenant() {
-	user := as.getLoggedInUser("nicoleta")
-	req := as.setupRequest(user, "/self/tenant")
-	res := req.Get()
-	as.Equal(200, res.Code)
-	var tenant = &models.Tenant{}
-	res.Bind(tenant)
-	as.Equal("Big Panther Technologies Inc.", tenant.Name)
+	as.LoadFixture("Tenant bootstrap")
+	var tests = []struct {
+		username   string
+		tenantName string
+	}{
+		{"mane", "Big Panther Liverpool"},
+		{"firmino", "Big Panther Liverpool"},
+		{"allan", "Big Panther Everton"},
+		{"salah", "Big Panther Liverpool"},
+		{"klopp", "Big Panther Test"},
+		{"adidas", "Big Panther Everton"},
+	}
+
+	for _, test := range tests {
+		as.T().Run(test.username, func(t *testing.T) {
+			user := as.getLoggedInUser(test.username)
+			req := as.setupRequest(user, "/self/tenant")
+			res := req.Get()
+			as.Equal(200, res.Code)
+			var tenant = &models.Tenant{}
+			res.Bind(tenant)
+			as.Equal(test.tenantName, tenant.Name)
+		})
+	}
 }
 
 func (as *ActionSuite) getLoggedInUser(username string) *models.User {
-	as.LoadFixture("Tenant bootstrap")
 	var user = &models.User{}
 	err := as.DB.Where("username=?", username).First(user)
 	as.NoError(err)
--- actions/tenants_test.go
+++ actions/tenants_test.go
@@ -1,22 +1,71 @@
 package actions
 
 import (
+	"fmt"
+	"testing"
+
 	"github.com/bigpanther/trober/models"
 )
 
 func (as *ActionSuite) Test_TenantsResource_List() {
-	user := as.getLoggedInUser("nicoleta")
-	req := as.setupRequest(user, "/tenants")
-	res := req.Get()
-	as.Equal(200, res.Code)
-	var tenants = &models.Tenants{}
-	res.Bind(tenants)
-	as.Equal(len(*tenants), 1)
+	as.LoadFixture("Tenant bootstrap")
+	var tests = []struct {
+		username     string
+		responseCode int
+		tenantCount  int
+	}{
+		{"mane", 404, 0},
+		{"firmino", 404, 0},
+		{"allan", 404, 0},
+		{"salah", 404, 0},
+		{"klopp", 200, 3},
+		{"adidas", 404, 0},
+	}
+
+	for _, test := range tests {
+		as.T().Run(test.username, func(t *testing.T) {
+			user := as.getLoggedInUser(test.username)
+			req := as.setupRequest(user, "/tenants")
+			res := req.Get()
+			as.Equal(test.responseCode, res.Code)
+			if test.responseCode == 200 {
+				var tenants = &models.Tenants{}
+				res.Bind(tenants)
+				as.Equal(len(*tenants), test.tenantCount)
+			}
+		})
+	}
 
 }
 
 func (as *ActionSuite) Test_TenantsResource_Show() {
-	as.False(false)
+	as.LoadFixture("Tenant bootstrap")
+	var tests = []struct {
+		username     string
+		responseCode int
+		tenantName   string
+	}{
+		{"mane", 200, "Big Panther Liverpool"},
+		{"firmino", 200, "Big Panther Liverpool"},
+		{"allan", 404, "Big Panther Everton"},
+		{"salah", 200, "Big Panther Liverpool"},
+		{"klopp", 200, "Big Panther Test"},
+		{"adidas", 200, "Big Panther Everton"},
+	}
+
+	for _, test := range tests {
+		as.T().Run(test.username, func(t *testing.T) {
+			user := as.getLoggedInUser(test.username)
+			req := as.setupRequest(user, fmt.Sprintf("/tenants/%s", user.TenantID))
+			res := req.Get()
+			as.Equal(test.responseCode, res.Code)
+			if test.responseCode == 200 {
+				var tenant = &models.Tenant{}
+				res.Bind(tenant)
+				as.Equal(test.tenantName, tenant.Name)
+			}
+		})
+	}
 }
 
 func (as *ActionSuite) Test_TenantsResource_Create() {

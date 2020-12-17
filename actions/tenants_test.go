aa196ec9e8b18287f2545d87e4f17809916795c4
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-16 21:20:18 -0800

    Update destroy test

--- actions/tenants_test.go
+++ actions/tenants_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/bigpanther/trober/models"
+	"github.com/gobuffalo/nulls"
 )
 
 func (as *ActionSuite) Test_TenantsResource_List() {
@@ -35,7 +36,6 @@ func (as *ActionSuite) Test_TenantsResource_List() {
 			}
 		})
 	}
-
 }
 
 func (as *ActionSuite) Test_TenantsResource_Show() {
@@ -82,5 +82,44 @@ func (as *ActionSuite) Test_TenantsResource_Update() {
 }
 
 func (as *ActionSuite) Test_TenantsResource_Destroy() {
-	as.False(false)
+	as.LoadFixture("Tenant bootstrap")
+	var tests = []struct {
+		username     string
+		responseCode int
+		tenantCount  int
+	}{
+		{"mane", 404, 0},
+		{"firmino", 404, 0},
+		{"allan", 404, 0},
+		{"salah", 404, 0},
+		{"adidas", 404, 0},
+		{"klopp", 200, 3},
+	}
+	newTenant := &models.Tenant{Name: "Test", Type: "Production", Code: nulls.NewString("someC")}
+	v, err := as.DB.ValidateAndCreate(newTenant)
+	as.Nil(err)
+	as.Equal(0, len(v.Errors))
+
+	for _, test := range tests {
+		as.T().Run(test.username, func(t *testing.T) {
+			user := as.getLoggedInUser(test.username)
+			req := as.setupRequest(user, fmt.Sprintf("/tenants/%s", newTenant.ID))
+			res := req.Delete()
+			as.Equal(test.responseCode, res.Code)
+			if test.responseCode == 200 {
+				var tenant = &models.Tenant{}
+				res.Bind(tenant)
+				as.Equal("Test", tenant.Name)
+				// Check if actually delted
+				tenant = &models.Tenant{}
+				err = as.DB.Where("name=?", "Test").First(tenant)
+				as.Contains(err.Error(), "no rows in result set")
+			} else {
+				tenant := &models.Tenant{}
+				err = as.DB.Where("name=?", "Test").First(tenant)
+				//Not deleted yet
+				as.Equal("Test", tenant.Name)
+			}
+		})
+	}
 }

ee98dc0eba4ae1f7a4ba12ba28e45dc013da3500
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-21 05:40:35 -0800

    Add validator tests. Test tasks

--- models/carrier_test.go
+++ models/carrier_test.go
@@ -1,5 +1,25 @@
 package models
 
+import (
+	"fmt"
+	"testing"
+)
+
 func (ms *ModelSuite) Test_Carrier() {
-	ms.False(false)
+	var tests = []struct {
+		carrier                  *Carrier
+		expectedValidationErrors int
+	}{
+		{&Carrier{}, 2},
+		{&Carrier{Type: "invalid"}, 2},
+		{&Carrier{Name: "some name"}, 1},
+		{&Carrier{Name: "some name", Type: CarrierTypeAir.String()}, 0},
+	}
+	for i, test := range tests {
+		ms.T().Run(fmt.Sprint(i), func(t *testing.T) {
+			v, err := test.carrier.Validate(ms.DB)
+			ms.Nil(err)
+			ms.Equal(test.expectedValidationErrors, len(v.Errors))
+		})
+	}
 }
--- models/order_test.go
+++ models/order_test.go
@@ -1,5 +1,25 @@
 package models
 
+import (
+	"fmt"
+	"testing"
+)
+
 func (ms *ModelSuite) TestOrder() {
-	ms.False(false)
+	var tests = []struct {
+		order                    *Order
+		expectedValidationErrors int
+	}{
+		{&Order{}, 2},
+		{&Order{Status: "invalid"}, 2},
+		{&Order{SerialNumber: "ORD0001"}, 1},
+		{&Order{SerialNumber: "ORD0001", Status: OrderStatusAccepted.String()}, 0},
+	}
+	for i, test := range tests {
+		ms.T().Run(fmt.Sprint(i), func(t *testing.T) {
+			v, err := test.order.Validate(ms.DB)
+			ms.Nil(err)
+			ms.Equal(test.expectedValidationErrors, len(v.Errors))
+		})
+	}
 }

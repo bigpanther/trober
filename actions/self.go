f0d9d3b48989f7b6a5a0a4b27cc3513ce3ca1d84
Harsimran Singh Maan <maan.harry@gmail.com>
2021-01-02 17:24:43 -0800

    Add device registration endpoints

--- actions/self.go
+++ actions/self.go
@@ -3,6 +3,7 @@ package actions
 import (
 	"net/http"
 
+	"github.com/bigpanther/trober/firebase"
 	"github.com/bigpanther/trober/models"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop/v5"
@@ -20,4 +21,32 @@ func selfGetTenant(c buffalo.Context) error {
 		return c.Error(http.StatusNotFound, err)
 	}
 	return c.Render(http.StatusOK, r.JSON(tenant))
+}
+
+func selfDeviceRegister(c buffalo.Context) error {
+	deviceB := deviceBody{}
+	if err := c.Bind(&deviceB); err != nil {
+		return err
+	}
+	if err := firebase.SubscribeToTopics(c, loggedInUser(c), deviceB.DeviceID); err != nil {
+		return err
+	}
+	c.Response().WriteHeader(http.StatusNoContent)
+	return nil
+}
+
+func selfDeviceRemove(c buffalo.Context) error {
+	deviceB := deviceBody{}
+	if err := c.Bind(&deviceB); err != nil {
+		return err
+	}
+	if err := firebase.UnSubscribeToTopics(c, loggedInUser(c), deviceB.DeviceID); err != nil {
+		return err
+	}
+	c.Response().WriteHeader(http.StatusNoContent)
+	return nil
+}
+
+type deviceBody struct {
+	DeviceID string `json:"device_id"`
 }
